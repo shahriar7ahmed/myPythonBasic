f6115e600d7d1a5f4932ef785a6e033553c7f477
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-11-30 16:59:46 +0600

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,23 @@
+#bfs in a graph
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return visited
+#input grpah
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+print(bfs(graph, 'A'))
+#output: ['A', 'B', 'C', 'D', 'E', 'F']
