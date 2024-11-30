8973bae24031b170b8e72ff21a4f9076b16069e2
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-11-30 16:16:11 +0600

    Add files via upload

---
+++ sqroot.py
@@ -0,0 +1,10 @@
+#square root of a number without using math module
+num = int(input("Enter a number: "))
+sqroot = num ** 0.5
+print("Square root of", num, "is", sqroot)
+#square root of a number using math module
+import math
+num = int(input("Enter a number: "))
+sqroot = math.sqrt(num)
+print("Square root of", num, "is", sqroot)
+
